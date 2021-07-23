d62125f3493bfc947fbfbdbc55025482023d600a
Roberto Leon-Luna <rsleonluna>
2021-07-23 17:15:56 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,39 @@ Player.create!(name: 'Kobe Bryant',
 	height: 78,
 	position: 'Guard',
 	image_url: 'kobe.jpg')
+Player.create!(name: 'LeBron James', 
+	height: 82,
+	position: 'Forward',
+	image_url: 'lebron.jpg')
+Player.create!(name: 'Tim Duncan', 
+	height: 84,
+	position: 'Forward',
+	image_url: 'timduncan.jpg')
+Player.create!(name: 'Michael Jordan', 
+	height: 78,
+	position: 'Guard',
+	image_url: 'mike.jpg')
+Player.create!(name: 'Bill Russell', 
+	height: 86,
+	position: 'Center',
+	image_url: 'billrussell.jpg')
+Player.create!(name: 'Larry Bird', 
+	height: 82,
+	position: 'Forward',
+	image_url: 'larry.jpg')
+Player.create!(name: 'Magic Johnson', 
+	height: 83,
+	position: 'Guard',
+	image_url: 'magic.jpg')
+Player.create!(name: 'Kareem Abdul-Jabbar', 
+	height: 85,
+	position: 'Center',
+	image_url: 'kareem.jpg')
+Player.create!(name: 'Stephen Curry', 
+	height: 72,
+	position: 'Guard',
+	image_url: 'steph.jpg')
+Player.create!(name: 'Kevin Durant', 
+	height: 86,
+	position: 'Forward',
+	image_url: 'kd.jpg')
